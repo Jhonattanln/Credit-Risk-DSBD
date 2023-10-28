dc1777ecc6d70ffd438139480781421dada5f9b2
Jhonattan <jhonattanlino@outlook.com>
2023-10-28 17:33:54 -0300

    code: add packages

--- Code.r
+++ Code.r
@@ -1,7 +1,16 @@
 # Libraries
-
+library(dyplr)
+library(ggplot2)
 # Read data
-dados <- read.delim("Credit-Risk-DSBD/Files/german.data-numeric")
-dados2 <- read.delim("Credit-Risk-DSBD/Files/german.data")
-summary(dados2)
-View(dados2)
+dados <- read.delim("Files/german.data", header = FALSE, sep = "")
+
+# Definingo features e target
+X <- dados[,1:20]
+y <- dados[,21]
+
+# Estruturando variáveis categóricas
+cat <- c("V1", "V3", "V4", "V6", "V7", "V9", "V10", "V12", "V14", "V15", "V17", "V19", "V20")
+X[,cat] <- lapply(X[,cat], factor)
+str(X)
+dim(X)
+dim(y)
