d8e34ae7bff624270ecb1d6bb59c3dad23593b64
Jhonattan <jhonattanlino@outlook.com>
2023-10-28 15:04:33 -0300

    config: add database to model

--- Code.r
+++ Code.r
@@ -1,1 +1,7 @@
 # Libraries
+
+# Read data
+dados <- read.delim("Credit-Risk-DSBD/Files/german.data-numeric")
+dados2 <- read.delim("Credit-Risk-DSBD/Files/german.data")
+summary(dados2)
+View(dados2)
